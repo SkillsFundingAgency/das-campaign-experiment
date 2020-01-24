faa1b1cda0a64c2e03a43a18e1542589347c5995
Matt Derry <matt.derry@digital.education.gov.uk>
2020-01-24 15:11:16 +0000

    Updates database schema

--- src/SFA.DAS.Experiment.Database/Tables/CitizenApplicationEvents.sql
+++ src/SFA.DAS.Experiment.Database/Tables/CitizenApplicationEvents.sql
@@ -1,24 +1,17 @@
 CREATE TABLE [dbo].[CitizenApplicationEvents](
 	[Id] [uniqueidentifier] NOT NULL,
-	[CitizenFlag] [bit] NOT NULL,
-	[CandidateFlag] [bit] NOT NULL,
-	[CandidateEmailAddress] [nvarchar](250) NOT NULL,
+	[EventDate] [datetime2](7) NOT NULL,
+	[EventType] [int] NOT NULL,
 	[CandidateId] [nvarchar](250) NOT NULL,
-	[CandidateFirstName] [nvarchar](250) NOT NULL,
-	[CandidateSurname] [nvarchar](250) NOT NULL,
-	[CandidateDetailsChangeDate] [datetime2](7) NULL,
-	[CandidateAccountDeletionDate] [datetime2](7) NULL,
+	[CandidateFirstName] [nvarchar](250) NULL,
+	[CandidateSurname] [nvarchar](250) NULL,
+	[CandidateEmailAddress] [nvarchar](250) NOT NULL,
 	[ApplicationId] [nvarchar](250) NULL,
-	[ApplicationStartDate] [datetime2](7) NULL,
-	[ApplicationSubmitDate] [datetime2](7) NULL,
-	[VacancyLevel] [int] NOT NULL,
-	[VacancyTitle] [nvarchar](250) NOT NULL,
-	[VacancyReference] [nvarchar](250) NOT NULL,
-	[VacancyCloseDate] [datetime2](7) NOT NULL,
-	[EmailCampaignName] [nvarchar](250) NULL,
-	[EmailSendDate] [datetime2](7) NULL,
-	[Ethnicity] [int] NOT NULL,
-	[EventType] [int] NOT NULL
+	[VacancyId] [nvarchar](250) NULL,
+	[VacancyTitle] [nvarchar](250) NULL,
+	[VacancyReference] [nvarchar](250) NULL,
+	[VacancyCloseDate] [datetime2](7) NULL,
+	
 ) ON [PRIMARY]
 GO
 
