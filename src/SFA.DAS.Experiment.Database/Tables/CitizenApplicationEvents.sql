771c052aaf579d988c05348926b51280e99eefcb
Dave Gouge <davegouge@gmail.com>
2020-02-03 13:43:31 +0000

    Updated table schema

--- src/SFA.DAS.Experiment.Database/Tables/CitizenApplicationEvents.sql
+++ src/SFA.DAS.Experiment.Database/Tables/CitizenApplicationEvents.sql
@@ -10,13 +10,20 @@
 	[VacancyId] [nvarchar](250) NULL,
 	[VacancyTitle] [nvarchar](250) NULL,
 	[VacancyReference] [nvarchar](250) NULL,
-	[VacancyCloseDate] [datetime2](7) NULL, 
-    CONSTRAINT [PK_CitizenApplicationEvents] PRIMARY KEY CLUSTERED ([Id]),
-	
-)
+	[VacancyCloseDate] [datetime2](7) NULL,
+	[Processed] [bit] NOT NULL,
+	[MarketoId] [int] NULL,
+ CONSTRAINT [PK_CitizenApplicationEvents] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
 ALTER TABLE [dbo].[CitizenApplicationEvents] ADD  CONSTRAINT [DF_Citizens_Id]  DEFAULT (newid()) FOR [Id]
+GO
+
+ALTER TABLE [dbo].[CitizenApplicationEvents] ADD  DEFAULT ((0)) FOR [Processed]
 GO
 
 
